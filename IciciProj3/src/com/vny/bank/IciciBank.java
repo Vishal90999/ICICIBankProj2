b85594ab0464b7d20ddea7c12f4c6e412b826884
VISHAL <VISHAL@DESKTOP-PC1SNN3>
2022-10-08 18:33:26 +0530

    commit by vishal...

---
+++ IciciProj3/src/com/vny/bank/IciciBank.java
@@ -0,0 +1,11 @@
+package com.vny.bank;
+
+public class IciciBank {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("18.32");
+		
+	}
+
+}
---
+++ IciciProj3/src/com/vny/test/BankTest.java
@@ -0,0 +1,9 @@
+package com.vny.test;
+
+public class BankTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
